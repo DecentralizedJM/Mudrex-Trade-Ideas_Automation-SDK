a4bf1608421dae5fd5aec5cc2a3ea58e77be3dab
Jithin Mohandas <decnetralizedjm@Jithins-MacBook-Air.local>
2026-01-22 13:35:24 +0530

    Fix: Support Python 3.9+ and fix dependency URL

--- setup.py
+++ setup.py
@@ -25,9 +25,11 @@ setup(
         "Topic :: Office/Business :: Financial :: Investment",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.11",
+    python_requires=">=3.9",
     install_requires=requirements,
     entry_points={
         "console_scripts": [
